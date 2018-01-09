5f472ae058166d9a28b45c3b0fab66b7a719f934
premalsingh <premals98@gmail.com>
2018-01-09 23:12:34 +0530

    Add files via upload

---
+++ movement3.py
@@ -0,0 +1,32 @@
+import csv
+binary=[]
+with open('test3.csv') as csvfile:
+    readC=csv.reader(csvfile,delimiter=',')
+    totalsum=0
+    for avgclc in readC:
+        totalsum=totalsum+int(avgclc[0])
+    totalavg=totalsum/60000
+with open('test3.csv') as csvfile:
+    readCSV=csv.reader(csvfile,delimiter=',')
+    j=0
+    i=0
+    sum=0
+    avg=0
+    for row in readCSV:
+        if j<250:
+           sum=sum+int(row[0])
+           j=j+1
+        elif j==250:
+            j=0
+            avg=sum/250
+            if(avg>totalavg):
+                print("Yes")
+                binary.append("Yes")
+            else:
+                print("No")
+                binary.append("No")
+            sum=0
+        i=i+1 
+csv=open("output3.csv","w")
+for bit in binary:
+    csv.write(bit+"\n") 
